f687a9c3c70ee72d827ba32ecbace2231ae52d66
Ian Hancock <ianhancock@Ians-MacBook-Pro.local>
2024-02-26 15:13:36 -0600

    wip

--- src/app/crud-form/crud-form.component.ts
+++ src/app/crud-form/crud-form.component.ts
@@ -1,19 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+export interface DailyTask {
+  gallonWater: boolean | null;
+  tenPages: boolean | null;
+  dailyWorkout: boolean | null;
+  decompressTime: boolean | null;
+  dailyTask: string | null;
+}
 
 @Component({
   selector: 'app-crud-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './crud-form.component.html',
   styleUrl: './crud-form.component.css',
 })
 export class CrudFormComponent {
-  dailyTasks = new FormGroup({
+  submittedValue: any
+  dailyTasksForm = new FormGroup({
     gallonWater: new FormControl(false),
     tenPages: new FormControl(false),
     dailyWorkout: new FormControl(false),
     decompressTime: new FormControl(false),
     dailyTask: new FormControl(''),
   });
+
+  onSubmit() {
+    this.submittedValue = this.dailyTasksForm.value;
+  }
 }
